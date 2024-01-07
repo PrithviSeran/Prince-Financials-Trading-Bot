917ebb259d207c374bd11364fed86cc286879002
prithviseran <109385760+PrithviSeran@users.noreply.github.com>
2024-01-07 14:23:45 -0500

    Add files via upload

---
+++ google_defs.py
@@ -0,0 +1,7 @@
+CLIENT_FILE = "prince_trading_project.json"
+SCOPES = ["https://www.googleapis.com/auth/cloud-platform"]
+
+GOOGLE_HEADER = {
+    'Authorization': f'Bearer {"ya29.a0AfB_byBdCvAQxJsYoiRRTmVqUuX1evIkDLZacVkCiVIixUIfZqekUObwIMOZUS4QbPq0TpYOe5xBYI-R-dzlgYKJGqll2kz0dkHvfKBGRu_A-IwsSHvHVdV3PwMJbDgDLJWzVV1vxHGqeZiJdxfEvfdGtyZVvMB64KCfaCgYKATESARESFQHGX2Mivgc9KrDzJAGWuliySMDS0w0171"}',
+    'Content-Type': 'application/zip'
+}
