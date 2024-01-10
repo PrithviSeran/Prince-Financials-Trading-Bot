a48a68081cf9b9c49dbd6db8e1bdaa8e6ca2e4ce
prithviseran <109385760+PrithviSeran@users.noreply.github.com>
2024-01-10 11:06:07 -0500

    Update main.tf

--- terras/main.tf
+++ terras/main.tf
@@ -1,10 +1,10 @@
 resource "google_storage_bucket" "buffer" {
-   name = "lets_test_twice"
+   name = "YOUR GOOGLE CLOUD STORAGE BUCKET NAME"
    location = "US"
 }
 
 resource "google_storage_bucket_object" "static_site_src"{
     name = "Trading_Bot.zip"
-    source = "/Users/prithviseran/Documents/Forex_Trading_Bot_Server/Trading_Bot.zip"
+    source = "THE ABSOLUTE PATH OF THE ZIPPED FILE OF 'Files_to_be_Imported"
     bucket = google_storage_bucket.buffer.name
 }
